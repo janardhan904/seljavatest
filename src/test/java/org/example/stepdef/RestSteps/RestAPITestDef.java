c0a2c99acdad929ce277356d7242a08c4f82a216
janardhan904 <janardhan.reddy75@gmail.com>
2021-07-13 02:16:08 +0100

    New  SKLTN

---
+++ src/test/java/org/example/stepdef/RestSteps/RestAPITestDef.java
@@ -0,0 +1,99 @@
+package org.example.stepdef.RestSteps;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class RestAPITestDef {
+
+    //GET REQUEST
+    @Test
+    public void GetRequest() {
+
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println(response.statusCode());
+        System.out.println(response.asString());
+        System.out.println(response.getBody().asString());
+        System.out.println(response.statusLine());
+
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+    }
+
+    @Test
+    public void VerifyGetRequest() {
+        given().get("https://reqres.in/api/users?page=2").then().statusCode(200).body("data.id[0]", equalTo(7));
+    }
+
+    @Test
+    public void VerifyGetRequestBody() {
+        given().get("https://reqres.in/api/users?page=2").then().
+                statusCode(200).
+                body("data.id[1]", equalTo(8)).
+                body("data.first_name", hasItems("Michael","Lindsay")).
+                log().all();
+    }
+
+    @Test
+    public void PostRequest() {
+        JSONObject request = new JSONObject();
+        request.put("name", "JD");
+        request.put("job", "TESTQA");
+
+        System.out.println(request);
+        System.out.println(request.toString());
+
+        given().
+                body(request.toJSONString()).
+                when().
+                post("https://reqres.in/api/users").
+                then().statusCode(201 );
+    }
+
+    @Test
+    public void PutRequestUpdate() {
+        JSONObject request = new JSONObject();
+        request.put("name", "JD");
+        request.put("job", "TESTQA");
+
+        System.out.println(request);
+        System.out.println(request.toString());
+
+        given().
+                body(request.toJSONString()).
+                when().
+                put("https://reqres.in/api/users/2").
+                then().statusCode(200);
+    }
+
+    @Test
+    public void PatchRequest() {
+        JSONObject request = new JSONObject();
+        request.put("name", "JD");
+        request.put("job", "TESTQA");
+
+        System.out.println(request);
+        System.out.println(request.toString());
+
+        given().
+                body(request.toJSONString()).
+                when().
+                patch("https://reqres.in/api/users").
+                then().statusCode(200);
+    }
+
+    @Test
+    public void DeleteRequest() {
+        JSONObject request = new JSONObject();
+        given().
+                body(request.toJSONString()).
+                when().
+                delete("https://reqres.in/api/users/2").
+                then().statusCode(204).
+                log().all();
+    }
+}
